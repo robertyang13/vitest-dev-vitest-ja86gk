725a6b18a07d2bfeb4722a1662105c2a0c0c3656
robertyang13 <91514950+robertyang13@users.noreply.github.com>
2024-05-21 21:48:11 -0500

    Update Timer.tsx

--- components/Timer.tsx
+++ components/Timer.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { formatDuration } from './duration.ts';
-interface TimerProps {}
-function Timer<TimerProps>(): any {
-  return <h1>Timer</h1>;
+interface TimerProps {
+  second: number;
+  callbackFunction: () => void;
+}
+function Timer<TimerProps>({ second, callbackFunction }): any {
+  const [seconds, setSeconds] = useState<number>(second);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds((prevSeconds) => {
+        if (prevSeconds === 0) {
+          clearInterval(intervalId);
+          callbackFunction();
+          return second;
+        } else {
+          return prevSeconds - 1;
+        }
+      });
+    }, 1000);
+  }, [second, callbackFunction]);
+
+  return <h1>Timer: {formatDuration(seconds)} </h1>;
 }
 export default Timer;
